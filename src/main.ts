534810d198bef2b23fd657a433be770ee5daef2c
thl <tanghailong@daoba.com>
2023-10-26 16:23:42 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+
+import './assets/index.scss'
+import config from './config'
+const app = document.querySelector<HTMLDivElement>('#app')!
+app.style.width = config.canvas.width + 'px'
+app.style.height = config.canvas.height + 'px'
